43f7ce3a27823f22792764d658897b56feb8308e
rukiyearican <rukiyearican@gmail.com>
2023-06-21 17:29:55 +0300

    added libraries

--- week4/ps/ShannonEntropy.java
+++ week4/ps/ShannonEntropy.java
@@ -4,7 +4,21 @@ public class ShannonEntropy {
         // sequence of n integers are between 1 and m
         int m = Integer.parseInt(args[0]);
 
+        int[] elements = new int[m+1];
 
+        int count = 0;
+
+        while(!StdIn.isEmpty()){
+            int i = StdIn.readInt();
+            elements[i]++;
+            count++;
+        }
+
+        double shannon = 0.0;
+
+        for (int i = 1; i < m; i++) {
+            if (elements[i] == 0;)
+        }
 
 
     }
