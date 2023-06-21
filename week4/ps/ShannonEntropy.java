c2bd014a935ee3dfdf10b625bf1f8f789807136b
rukiyearican <rukiyearican@gmail.com>
2023-06-21 17:16:55 +0300

    starting ps

---
+++ week4/ps/ShannonEntropy.java
@@ -0,0 +1,11 @@
+public class ShannonEntropy {
+    public static void main(String[] args) {
+
+        // sequence of n integers are between 1 and m
+        int m = Integer.parseInt(args[0]);
+
+
+
+
+    }
+}
