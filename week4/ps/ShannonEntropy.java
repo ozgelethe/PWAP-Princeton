e64f91389196f7c309e6c63f6e6a88817d733c76
rukiyearican <rukiyearican@gmail.com>
2023-07-16 15:28:56 +0300

    finished shaannon

--- week4/ps/ShannonEntropy.java
+++ week4/ps/ShannonEntropy.java
@@ -4,28 +4,28 @@ public class ShannonEntropy {
         // sequence of n integers are between 1 and m
         int m = Integer.parseInt(args[0]);
 
-        int[] elements = new int[m+1];
+        int[] values = new int[m+1];
 
-        int count = 0;
-
-        while(!StdIn.isEmpty()){
+        // compute frequencies that is the times of integer i appears
+        int freq = 0;
+ 
+        while(!(StdIn.isEmpty()))
+        {
             int i = StdIn.readInt();
-            elements[i]++;
-            count++;
+            values[i]++;
+            freq++;
         }
 
+        // compute the shannon entropy
         double shannon = 0.0;
 
-        for (int i = 1; i < m; i++) {
-            if (elements[i] == 0) {shannon = shannon + 0.0;}
-            else {
-                //System.out.println("value:" +values[i]);
-                double p = (double) elements[i]/count;
-                //System.out.println("probability:" + p);
+        for (int i = 1; i <= m; i++) 
+        {
+            if (!(values[i] == 0))
+            {
+                double p = (double)values[i]/freq;
                 double temp = (p) * (Math.log(p) / Math.log(2));
-                //System.out.println("temp =" + temp);
                 shannon = shannon - temp;
-
             }
         }
         StdOut.printf("%.4f\n", shannon);
