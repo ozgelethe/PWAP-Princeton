c67f6fb8b3652d2be78e9f000ceee5e5a2d96c7f
rukiyearican <rukiyearican@gmail.com>
2023-07-16 15:54:46 +0300

    finished Checkerboard

--- week4/ps/Checkerboard.java
+++ week4/ps/Checkerboard.java
@@ -1,3 +1,32 @@
 public class Checkerboard {
-    
+    public static void main(String[] args) {
+
+        int n = Integer.parseInt(args[0]);
+
+        // draw main board
+        StdDraw.setXscale(0, n);
+        StdDraw.setYscale(0, n);
+
+
+        // color the board
+        for (int i = 0; i < n; i++) 
+        {
+            for (int j = 0; j < n; j++) 
+            {
+                if ((i + j) % 2 != 0)
+                {
+                    StdDraw.setPenColor(StdDraw.LIGHT_GRAY);
+                }
+                else
+                {
+                    StdDraw.setPenColor(StdDraw.BLUE);
+                }
+                StdDraw.filledSquare(i + 0.5, j + 0.5, 0.5);
+            }
+        }
+
+
+
+
+    }
 }
