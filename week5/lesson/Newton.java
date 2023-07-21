f1602b798b02c2b19ade668ec0f6bc6588a5150b
rukiyearican <rukiyearican@gmail.com>
2023-07-21 12:18:09 +0300

    starting to week5

---
+++ week5/lesson/Newton.java
@@ -0,0 +1,24 @@
+public class Newton {
+    public static double sqrt(double c, double eps) {
+        if (c < 0)
+            return Double.NaN;
+        double t = c;
+        while (Math.abs(t - c / t) < eps * t) {
+            t = (c / t + t) / 2.0;
+        }
+        return t;
+    }
+
+    public static void main(String[] args) 
+    {
+        double[] a = new double[args.length];
+        for (int i = 0; i < args.length; i++)
+        {
+            a[i] = Double.parseDouble(args[i]);
+        }
+        for (int i = 0; i < a.length; i++)
+        {
+            StdOut.println(sqrt(a[i], 1e-3));
+        }
+    }
+}
