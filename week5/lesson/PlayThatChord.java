91321c371de057a60110ffaa776d37990d6552a1
rukiyearican <rukiyearican@gmail.com>
2023-07-24 15:12:54 +0300

    added play chord function

--- week5/lesson/PlayThatChord.java
+++ week5/lesson/PlayThatChord.java
@@ -6,4 +6,23 @@ public class PlayThatChord {
         }
         return c;
     }
+
+    public static double[] chord(int pitch1, int pitch2, double d)
+     {
+        double hz1 = 440.0 * Math.pow(2, pitch1 / 12.0);
+        double hz2 = 440.0 * Math.pow(2, pitch2 / 12.0);
+        double[] a = PlayThatNote.tone(hz1, d);
+        double[] b = PlayThatNote.tone(hz2, d);
+        return avg(a,b);
+     }
+
+     public static void main(String[] args)
+     {
+        int pitch1 = Integer.parseInt(args[0]);
+        int pitch2 = Integer.parseInt(args[1]);
+        double duration = Double.parseDouble(args[2]);
+        double[] a = chord(pitch1, pitch2, duration);
+        StdAudio.play(a);
+     }
+
 }
