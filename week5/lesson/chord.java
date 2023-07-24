56a20a7d753a29ab2edc00349c11aa11f45dcc2d
rukiyearican <rukiyearican@gmail.com>
2023-07-24 15:05:51 +0300

    added chord

---
+++ week5/lesson/chord.java
@@ -0,0 +1,11 @@
+public class chord {
+    public static double[] avg(double[] a, double[] b)
+    {
+        double[] c = new double[a.length];
+        for (int i = 0; i < a.length; i++)
+        {
+            c[i] = a[i] /2.0 + b[i]/2.0;
+        }
+        return c;
+    }
+}
