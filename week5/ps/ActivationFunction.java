36237ee86a505cb6961dc087ad427699dac022ef
rukiyearican <rukiyearican@gmail.com>
2023-08-02 13:57:44 +0300

    added divisiors

--- week5/ps/ActivationFunction.java
+++ week5/ps/ActivationFunction.java
@@ -46,5 +46,30 @@ public class ActivationFunction
     {
         // All activation functions should return NaN (not a number) 
         // if the argument is NaN.
+        double x = Double.parseDouble((args[0]));
+        double heave, sig, tan, soft, sqnl;
+        if (Double.isNaN(x))
+        {
+            heave = Double.NaN;
+            sig = Double.NaN;
+            tan = Double.NaN;
+            soft = Double.NaN;
+            sqnl = Double.NaN;
+        }
+
+        else 
+        {
+            heave = heaviside(x);
+            sig = sigmoid(x);
+            tan = tanh(x);
+            soft = softsign(x);
+            sqnl = sqnl(x);
+        }
+
+        System.out.printf("%1$10s%2$s%3$s%4$s%n", "heaveside(", x, ") = ", heave);
+        System.out.printf("%1$10s%2$s%3$s%4$s%n", "sigmoid(", x, ") = ", sig);
+        System.out.printf("%1$10s%2$s%3$s%4$s%n", "tanh(", x, ") = ", tan);
+        System.out.printf("%1$10s%2$s%3$s%4$s%n", "softsign(", x, ") = ", soft);
+        System.out.printf("%1$10s%2$s%3$s%4$s%n", "sqnl(", x, ") = ", sqnl);
     }
 }
---
+++ week5/ps/Divisors.java
@@ -0,0 +1,30 @@
+public class Divisors
+{
+    // returns the greatest common divisor of a and b
+    public static int gcd(int a, int b)
+    {
+        if (a == 0 && b == 0)
+        {
+            return 0;
+        }
+    }
+
+    // returns the least common multiple of a and b
+    public static int lcm(int a, int b)
+
+    //returns true if a and b are relatively prime; false otherwise
+    public static boolean areRelativelyPrime(int a, int b)
+
+    // retuns the number of integers betwwen 1 and n that are
+    // relatively prime with n
+    public static int totient(int n)
+
+    // takes two integers command-line arguments a and b and prints
+    // each function, evaluated in the format and order
+    public static void main(String[] args)
+    {
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+    }
+
+}
